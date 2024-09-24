17e8677f43639a105559bb519c17ed8308a4f6c8
jtan0435 <jtan0435@student.monash.edu>
2024-09-24 22:08:10 +1000

    Updated add package component.

--- frontend/src/app/add-package/add-package.component.ts
+++ frontend/src/app/add-package/add-package.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { PackageService } from '../package.service';
+import { Router } from "@angular/router";
+import { Package } from '../models/package';
 
 @Component({
   selector: 'app-add-package',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-package.component.html',
   styleUrl: './add-package.component.css'
 })
 export class AddPackageComponent {
 
+  package: Package = new Package();
+
+  constructor(private packages: PackageService, private router: Router) { }
+
+  addPackage() {
+    this.packages.addPackage(this.package).subscribe(() => {
+      this.router.navigate(['/packages']);
+    });
+  }
 }
