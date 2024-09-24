9837741f8a27342a4f4e5e44ccc27f8ebeaefec3
jtan0435 <jtan0435@student.monash.edu>
2024-09-24 22:08:23 +1000

    Updated delete driver component.

--- frontend/src/app/delete-driver/delete-driver.component.ts
+++ frontend/src/app/delete-driver/delete-driver.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DriverService } from '../driver.service';
+import { Router } from '@angular/router';
 
-@Component({
+@Component({      
   selector: 'app-delete-driver',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './delete-driver.component.html',
   styleUrl: './delete-driver.component.css'
 })
 export class DeleteDriverComponent {
 
+  driver = { _id: '' }; 
+
+  constructor(private driverDB: DriverService, private router: Router) {}
+
+  deleteDriver() {
+    const driverId = this.driver._id;
+    if (driverId) {
+     this.driverDB.removeDriver(driverId).subscribe(
+        (response: any) => {
+          if (response.deletedCount > 0) {
+            this.router.navigate(['/list_drivers']);
+          } else {
+            this.router.navigate(['/invalid_data']);
+          }
+        },
+        error => {
+          this.router.navigate(['/invalid_data']);
+        }
+      );
+    }
+  }
 }
