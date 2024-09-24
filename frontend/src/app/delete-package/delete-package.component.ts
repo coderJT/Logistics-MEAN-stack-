adaa784152f2d7fc6ab79ff8451e01c4a39b9d98
jtan0435 <jtan0435@student.monash.edu>
2024-09-24 22:08:51 +1000

    Updated delete package component.

--- frontend/src/app/delete-package/delete-package.component.ts
+++ frontend/src/app/delete-package/delete-package.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PackageService } from '../package.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-delete-package',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './delete-package.component.html',
   styleUrl: './delete-package.component.css'
 })
 export class DeletePackageComponent {
 
+  package = { _id: '' };
+
+  constructor(private packageDB: PackageService, private router: Router) { } 
+
+
+  deletePackage() {
+    const packageId = this.package._id;
+    if (packageId) {
+     this.packageDB.removePackage(packageId).subscribe(
+        (response: any) => {
+          if (response.deletedCount > 0) {
+            this.router.navigate(['/list_packages']);
+          } else {
+            this.router.navigate(['/invalid_data']);
+          }
+        },
+        error => {
+          this.router.navigate(['/invalid_data']);
+        }
+      );
+    }
+  }
 }
