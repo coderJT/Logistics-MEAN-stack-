f676664c5943595cdb65eb0ab407293889d12a60
jtan0435 <jtan0435@student.monash.edu>
2024-09-24 22:12:18 +1000

    Updated statistics component.

--- frontend/src/app/statistics/statistics.component.ts
+++ frontend/src/app/statistics/statistics.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatisticsService } from '../statistics.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-statistics',
   standalone: true,
   imports: [],
   templateUrl: './statistics.component.html',
-  styleUrl: './statistics.component.css'
+  styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+  createCount: number = 0;
+  readCount: number = 0;
+  updateCount: number = 0;
+  deleteCount: number = 0;
 
+  constructor(private statisticsService: StatisticsService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadStatistics();
+  }
+
+  loadStatistics() {
+    this.statisticsService.getStatistics().subscribe(
+      (response: any) => {
+        this.createCount = response.createCount;
+        this.readCount = response.readCount;
+        this.updateCount = response.updateCount;
+        this.deleteCount = response.deleteCount;
+      },
+      error => {
+        console.error('Failed to load statistics:', error);
+        this.router.navigate(['/invalid_data']); 
+      }
+    );
+  }
 }
