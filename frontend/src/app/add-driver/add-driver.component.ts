e1f0b6fd93a4b0423ed2f5183fdbc40f11326ff5
jtan0435 <jtan0435@student.monash.edu>
2024-09-24 22:07:56 +1000

    Updated add driver component.

--- frontend/src/app/add-driver/add-driver.component.ts
+++ frontend/src/app/add-driver/add-driver.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DriverService } from '../driver.service';
+import { Router } from '@angular/router';
+import { Driver } from '../models/driver';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-driver',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './add-driver.component.html',
   styleUrl: './add-driver.component.css'
 })
 export class AddDriverComponent {
 
+  driver: Driver = new Driver();
+
+  constructor(private drivers: DriverService, private router: Router) {}
+
+  addDriver() {
+    this.drivers.addDriver(this.driver);
+    this.router.navigate(['/drivers']);
+  }
 }
