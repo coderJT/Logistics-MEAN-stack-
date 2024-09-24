868bc2f0da80e862bd41031a6b5e304e9c0038c7
jtan0435 <jtan0435@student.monash.edu>
2024-09-24 23:44:24 +1000

    Fixed link issue with add package component.

--- frontend/src/app/add-package/add-package.component.ts
+++ frontend/src/app/add-package/add-package.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { DriverService } from '../driver.service';
 import { PackageService } from '../package.service';
 import { Router } from "@angular/router";
 import { Package } from '../models/package';
@@ -14,12 +15,27 @@ import { Package } from '../models/package';
 export class AddPackageComponent {
 
   package: Package = new Package();
+  drivers: any[] = [];
 
-  constructor(private packages: PackageService, private router: Router) { }
+  constructor(private driverDB: DriverService, private packageDB: PackageService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadDrivers(); 
+  }
+
+  loadDrivers() {
+    this.driverDB.getDrivers().subscribe((data: any) => {
+      this.drivers = data;
+    }, (error) => {
+      console.error('Error fetching drivers', error);
+    });
+  }
+
 
   addPackage() {
-    this.packages.addPackage(this.package).subscribe(() => {
+    this.packageDB.addPackage(this.package).subscribe(() => {
       this.router.navigate(['/packages']);
     });
   }
+
 }
