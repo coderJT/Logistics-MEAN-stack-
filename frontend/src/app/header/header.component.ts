cd4a8878570b5de80d9d7acd50a94a4c029834d0
jtan0435 <jtan0435@student.monash.edu>
2024-09-26 00:40:11 +1000

    Modified source link for images.

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
   userLoggedIn = false;
+
+  constructor(private authService: AuthenticationService, private router: Router) { 
+    this.userLoggedIn = this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+      this.userLoggedIn = false;
+      this.router.navigate(['/login']); 
+    });
+  }
 }
