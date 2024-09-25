5c10df6f7adde020a0487fc02b74a74850b1e72e
jtan0435 <jtan0435@student.monash.edu>
2024-09-26 00:37:35 +1000

    Added authentiation for statistics service.,

--- frontend/src/app/statistics.service.ts
+++ frontend/src/app/statistics.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service'; // Add if headers are needed
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,10 @@ export class StatisticsService {
 
   private apiUrl = 'http://localhost:8080/api/v1/statistics';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthenticationService) { }
 
-  getStatistics() {
-    return this.http.get(this.apiUrl);
+  getStatistics(): Observable<any> {
+    const headers = this.authService.setAuthHeaders(); 
+    return this.http.get<any>(this.apiUrl, { headers });
   }
 }
