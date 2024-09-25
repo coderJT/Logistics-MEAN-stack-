061dbbe0c196a8788f07ec3db3809e324d3916ff
jtan0435 <jtan0435@student.monash.edu>
2024-09-25 15:08:32 +1000

    Updated routes to include list drivers by department component.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,11 +11,13 @@ import { UpdatePackageComponent } from './update-package/update-package.componen
 import { StatisticsComponent } from './statistics/statistics.component';
 import { InvalidDataComponent } from './invalid-data/invalid-data.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ListDriversByDepartmentComponent } from './list-drivers-by-department/list-drivers-by-department.component';
 
 export const routes: Routes = [
     { path: "", component: HomepageComponent},
     { path: 'add-driver', component: AddDriverComponent },
     { path: 'list-drivers', component: ListDriversComponent },
+    { path: 'list-drivers-by-department', component: ListDriversByDepartmentComponent },
     { path: 'delete-driver', component: DeleteDriverComponent },
     { path: 'update-driver', component: UpdateDriverComponent },
     { path: 'add-package', component: AddPackageComponent },
