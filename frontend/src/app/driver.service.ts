d05a1cbd3a4be387575afc817822bc19f6c6b32a
jtan0435 <jtan0435@student.monash.edu>
2024-09-25 15:09:27 +1000

    Modified driver service to implement getDriversByDepartment method.

--- frontend/src/app/driver.service.ts
+++ frontend/src/app/driver.service.ts
@@ -1,17 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type': 'application/json'
+//   })
+// }
+
 export class DriverService {
+  
   private apiUrl = 'http://localhost:8080/api/v1/drivers';
 
   constructor(private http: HttpClient) { }
 
   getDrivers() {
     return this.http.get(this.apiUrl);
-  }
+  } 
 
   addDriver(driver: any) {
     return this.http.post(this.apiUrl, driver);
@@ -23,6 +31,10 @@ export class DriverService {
 
   removeDriver(id: string) {
     return this.http.delete(`${this.apiUrl}/?_id=${id}`);
+  }
+
+  getDriversByDepartment(driver_department: string) {
+    return this.http.get(`${this.apiUrl} + /department/${driver_department}`);
   }
 
 }
