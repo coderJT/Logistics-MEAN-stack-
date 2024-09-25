4b81e1502f7482759a2a6c485f67f409c5b2d347
jtan0435 <jtan0435@student.monash.edu>
2024-09-25 15:13:03 +1000

    Updated driver model to include _id and created_at attributes.

--- frontend/src/app/models/driver.ts
+++ frontend/src/app/models/driver.ts
@@ -1,17 +1,21 @@
 export class Driver {
+    _id: string;
     driver_id: string;
     driver_name: string;
     driver_department: string;
     driver_license: string;
     driver_is_active: boolean;
     assigned_packages: string[];  
+    created_at: Date;
     
     constructor() {
+        this._id = "";
         this.driver_id = "";
         this.driver_name = "";
         this.driver_department = "";
         this.driver_license = "";
         this.driver_is_active = false;
         this.assigned_packages = [];
+        this.created_at = new Date();
     }   
 }
