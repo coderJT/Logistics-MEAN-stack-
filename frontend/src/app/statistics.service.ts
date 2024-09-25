d8b5b6009f7e3f17e9e15f14f02f62be15ce5414
jtan0435 <jtan0435@student.monash.edu>
2024-09-25 15:10:13 +1000

    Modified statistics component to use localhost:8080.

--- frontend/src/app/statistics.service.ts
+++ frontend/src/app/statistics.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class StatisticsService {
 
-  private apiUrl = 'http://localhost:3000/api/v1/statistics';
+  private apiUrl = 'http://localhost:8080/api/v1/statistics';
 
   constructor(private http: HttpClient) { }
 
