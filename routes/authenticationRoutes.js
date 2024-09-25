0884c5166fe412c177d27a7f5c1eb3cf6ef20635
jtan0435 <jtan0435@student.monash.edu>
2024-09-26 09:16:24 +1000

    Fixed login and logout logic for authentication routes.

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -26,127 +26,11 @@ const db = admin.firestore();
 const bcrypt = require('bcrypt');
 
 /**
- * GET /signup
- * 
- * @description Renders a page to let user sign up.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.get('/signup', async (req, res) => {
-    res.render('signup', {
-        userLoggedIn: !!req.session.user,
-    });
-});
-
-/**
- * GET /login
- * 
- * @description Renders a page to let user log in.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.get('/login', async (req, res) => {
-    res.render('login', {
-        userLoggedIn: !!req.session.user,
-    });
-});
-
-/**
- * POST /signup
- * 
- * @description Sign up a user. 
- * @description In the case that user entered wrong password during confirmation, redirect user to invalid data page.
- * @description If user logged in successfully, redirect user to login page.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.post('/signup', async (req, res) => {
-    const { username, password, confirmPassword } = req.body;
-
-    if (password !== confirmPassword) {
-        return res.redirect('invalid_data');
-    }
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await db.collection('users').doc(username).set({
-            username,
-            password: hashedPassword
-        });
-
-        res.redirect('login');
-
-    } catch (error) {
-        res.redirect('invalid_data');
-    }
-});
-
-/**
- * POST /login
- * 
- * @description Logs in a user. 
- * @description In the case that user doesnt exist or password is incorrect, redirect user to invalid data page.
- * @description If user logged in successfully, update req.session.user to keep track of login status.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const userDoc = await db.collection('users').doc(username).get();
-
-        if (!userDoc.exists) {
-            return res.redirect('invalid_data'); 
-        }
-
-        const userData = userDoc.data();
-        const isMatch = await bcrypt.compare(password, userData.password);
-
-        if (!isMatch) {
-            return res.redirect('invalid_data'); 
-        }
-
-        req.session.user = { username }; 
-        res.redirect('/');
-    } catch (error) {
-        res.redirect('invalid_data');
-    }
-});
-
-/**
- * GET /logout
- * 
- * @description Logs out current user. In the case that logout 'fails', redirect user to home page.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).send('Logout Failed');
-        }
-        res.redirect('/');
-    })
-})
-
-/**
  * API Routes
  */
 
 /**
- * GET /api/v1/signup
+ * POST /api/v1/signup
  * 
  * @description Sign up a user through API.
  * 
@@ -184,7 +68,7 @@ router.post('/api/v1/signup', async (req, res) => {
 });
 
 /**
- * GET /api/v1/login
+ * POST /api/v1/login
  * 
  * @description Log in a user through API.
  * 
@@ -200,21 +84,23 @@ router.post('/api/v1/login', async (req, res) => {
         const userDoc = await db.collection('users').doc(username).get();
 
         if (!userDoc.exists) {
-            return res.redirect('invalid_data'); 
+            return res.status(404).json({
+                "status": "User not found",
+            })
         }
 
         const userData = userDoc.data();
         const isMatch = await bcrypt.compare(password, userData.password);
 
         if (!isMatch) {    
-            res.json({
+            res.status(401).json({
                 "status": "Password incorrect.",
             })
         }
         
         req.session.user = { username }; 
 
-        res.json({
+        res.status(200).json({
             "status": "Logged in successfully."
         })
 
@@ -249,7 +135,6 @@ router.get('/api/v1/logout', (req, res) => {
         })
     })
 })
-
 
 
 module.exports = router;
