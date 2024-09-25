305db3b112caf8fa717b33e63497b75eb34b1550
jtan0435 <jtan0435@student.monash.edu>
2024-09-26 00:37:14 +1000

    Added strict type checking for package service.

--- frontend/src/app/package.service.ts
+++ frontend/src/app/package.service.ts
@@ -1,28 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PackageService {
 
-  private apiUrl = 'http://localhost:8080/api/v1/drivers';
+  private apiUrl = 'http://localhost:8080/api/v1/packages'; // Adjusted endpoint to reflect packages
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authentication: AuthenticationService) { }
 
-  getPackages() {
-    return this.http.get(this.apiUrl);
+  private getHeaders(): HttpHeaders {
+    return this.authentication.setAuthHeaders();
   }
 
-  addPackage(packageInfo: any) {
-    return this.http.post(this.apiUrl, packageInfo);
+  getPackages(): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.get<any>(this.apiUrl, { headers });
   }
 
-  removePackage(packageId: string) {
-    return this.http.delete(`${this.apiUrl}/:id=${packageId}`);
+  addPackage(packageInfo: any): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.post<any>(this.apiUrl, packageInfo, { headers });
   }
 
-  updatePackage(packageInfo: any) {
-    return this.http.put(`${this.apiUrl}/`, packageInfo);
+  removePackage(packageId: string): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.delete<any>(`${this.apiUrl}/${packageId}`, { headers });
+  }
+
+  updatePackage(packageId: string, packageInfo: any): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.put<any>(`${this.apiUrl}/${packageId}`, packageInfo, { headers });
   }
 }
