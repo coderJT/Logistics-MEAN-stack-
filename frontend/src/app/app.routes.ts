053c0692ea1ef68ceea60be1de1206d9568db738
jtan0435 <jtan0435@student.monash.edu>
2024-09-26 00:36:14 +1000

    Added routing support for login, signup and added authentication guard.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -12,19 +12,24 @@ import { StatisticsComponent } from './statistics/statistics.component';
 import { InvalidDataComponent } from './invalid-data/invalid-data.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ListDriversByDepartmentComponent } from './list-drivers-by-department/list-drivers-by-department.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { authenticationGuard } from './guards/authentication.guard';  
 
 export const routes: Routes = [
-    { path: "", component: HomepageComponent},
-    { path: 'add-driver', component: AddDriverComponent },
-    { path: 'list-drivers', component: ListDriversComponent },
-    { path: 'list-drivers-by-department', component: ListDriversByDepartmentComponent },
-    { path: 'delete-driver', component: DeleteDriverComponent },
-    { path: 'update-driver', component: UpdateDriverComponent },
-    { path: 'add-package', component: AddPackageComponent },
-    { path: 'list-packages', component: ListPackagesComponent },
-    { path: 'delete-package', component: DeletePackageComponent },
-    { path: 'update-package', component: UpdatePackageComponent },
-    { path: 'statistics', component: StatisticsComponent },
+    { path: "", component: HomepageComponent },
+    { path: 'add-driver', component: AddDriverComponent, canActivate: [authenticationGuard] },
+    { path: 'list-drivers', component: ListDriversComponent, canActivate: [authenticationGuard] },
+    { path: 'list-drivers-by-department', component: ListDriversByDepartmentComponent, canActivate: [authenticationGuard] },
+    { path: 'delete-driver', component: DeleteDriverComponent, canActivate: [authenticationGuard] },
+    { path: 'update-driver', component: UpdateDriverComponent, canActivate: [authenticationGuard] },
+    { path: 'add-package', component: AddPackageComponent, canActivate: [authenticationGuard] },
+    { path: 'list-packages', component: ListPackagesComponent, canActivate: [authenticationGuard] },
+    { path: 'delete-package', component: DeletePackageComponent, canActivate: [authenticationGuard] },
+    { path: 'update-package', component: UpdatePackageComponent, canActivate: [authenticationGuard] },
+    { path: 'statistics', component: StatisticsComponent, canActivate: [authenticationGuard] },
     { path: 'invalid-data', component: InvalidDataComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },  
     { path: '**', component: PageNotFoundComponent },
 ];
