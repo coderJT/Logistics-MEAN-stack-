c9dc8496cfa0a61ff7d44fd19aa4eccccc8183bc
jtan0435 <jtan0435@student.monash.edu>
2024-09-27 11:16:45 +1000

    Implemeneted both update driver and update package component.

--- frontend/src/app/update-driver/update-driver.component.ts
+++ frontend/src/app/update-driver/update-driver.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DriverService } from '../driver.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-driver',
-  standalone: true,
-  imports: [],
   templateUrl: './update-driver.component.html',
-  styleUrl: './update-driver.component.css'
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  styleUrls: ['./update-driver.component.css']
 })
-export class UpdateDriverComponent {
+export class UpdateDriverComponent implements OnInit {
+  drivers: any[] = [];
+  selectedDriverId: string = "";
+  driver: any = null;
 
+  constructor(private driversDB: DriverService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.driversDB.getDrivers().subscribe((response: any) => {
+      for (let driver of response) {
+        this.drivers.push(driver);
+      }
+    })
+  }
+
+  loadDriverDetails() {
+    if (this.selectedDriverId) {
+      this.driversDB.getDriverById(this.selectedDriverId).subscribe((data: any) => {
+        this.driver = data; 
+      });
+    }
+  }
+
+  updateDriver() {
+    const updatedDriver = {
+      driver_department: this.driver.driver_department,
+      driver_license: this.driver.driver_license
+    };
+
+    this.driversDB.updateDriver(this.selectedDriverId, updatedDriver).subscribe(response => {
+      console.log('Driver updated successfully!', response);
+      this.router.navigate(['list-drivers'])
+    });
+  }
 }
--- frontend/src/app/update-package/update-package.component.ts
+++ frontend/src/app/update-package/update-package.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { PackageService } from '../package.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-package',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './update-package.component.html',
   styleUrl: './update-package.component.css'
 })
 export class UpdatePackageComponent {
+  packages: any[] = [];
+  selectedPackageId: string = "";
+  package: any = null;
 
+  constructor(private packagesDB: PackageService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.packagesDB.getPackages().subscribe(
+      (response: any) => {
+        for (let packageObj of response) {
+          this.packages.push(packageObj);
+        }
+      }
+    )
+  }
+
+  loadPackageDetails() {
+    if (this.selectedPackageId) {
+      this.packagesDB.getPackageById(this.selectedPackageId).subscribe((data: any) => {
+        this.package = data;
+      })
+    }
+  }
+
+  updatePackage() {
+    const updatedPackage = {
+      package_destination: this.package.package_destination,
+    };
+
+    this.packagesDB.updatePackage(this.selectedPackageId, updatedPackage).subscribe(response => {
+      console.log('Package updated successfully!', response);
+      this.router.navigate(['list-packages'])
+    })
+
+  }
 }
