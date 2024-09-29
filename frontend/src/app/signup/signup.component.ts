1bd3ae1905a4ae6de1c5a1d6e62ce01f6042a277
jtan0435 <jtan0435@student.monash.edu>
2024-09-29 11:53:16 +1000

    Fixed signup issues.

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -6,12 +6,11 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule], 
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css'] 
 })
 export class SignupComponent {
-
   username: string = "";
   password: string = "";
   confirmPassword: string = "";
@@ -19,22 +18,27 @@ export class SignupComponent {
   constructor(private authService: AuthenticationService, private router: Router) { }
 
   onSubmit() {
-    if (this.password !== this.confirmPassword) { 
-      console.error("Passwords do not match");  
+    if (this.password !== this.confirmPassword) {
+      console.log(this.password);
+      console.log(this.confirmPassword)
+      alert("Passwords do not match!");  
+      return;  
     }
 
     const credentials = {
       username: this.username,
       password: this.password,
-    }
+    };
 
     this.authService.signUp(credentials).subscribe(
       (response: any) => {
+        console.log('Signup successful');
         this.router.navigate(['/login']);
       },
       error => {
         console.error('Failed to signup:', error);
+        alert('Signup failed. Please try again.');  
       }
-    )
+    );
   }
 }
