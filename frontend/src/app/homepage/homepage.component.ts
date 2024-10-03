bd0c032adf7e1f6a08fb91ef920702f8ee95d789
jtan0435 <jtan0435@student.monash.edu>
2024-10-03 18:47:47 +1000

    Added comments and reviewed home page component.

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -8,17 +8,39 @@ import { UtilsService } from '../utils.service';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'] 
 })
-export class HomepageComponent implements OnInit {
+
+/**
+ * This component is responsible for displaying the home page.
+ * 
+ * This component will display the number of drivers and packages to the user.
+ * 
+ * @class HomepageComponent
+ */
+export class HomepageComponent {
+
+  /**
+   * This is the driver count.
+   */
   driverCount: number = 0;  
+
+  /**
+   * This is the package count.
+   */
   packageCount: number = 0;
 
+  /**
+   * Constructor that injects the UtilsService for handling driver and package count operations.
+   * 
+   * @param {UtilsService} utilsService - Service used to perform utility operations.
+   */
   constructor(private utilsService: UtilsService) {}
 
+  /**
+   * Load the driver and package counts on initialization.
+   * 
+   * @returns {void}
+   */
   ngOnInit(): void {
-    this.loadCount();
-  }
-
-  loadCount(): void {
     this.utilsService.getCount().subscribe(
       (response: any) => {
         this.driverCount = response.driverCount;
