f3f7c2a146b70451d06b2ba412b0eaf09272bf4d
jtan0435 <jtan0435@student.monash.edu>
2024-10-03 18:34:25 +1000

    Added comments and reviewed delete driver component.

--- frontend/src/app/delete-driver/delete-driver.component.ts
+++ frontend/src/app/delete-driver/delete-driver.component.ts
@@ -2,22 +2,48 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DriverService } from '../driver.service';
 import { Router } from '@angular/router';
+import { Driver } from '../models/driver';
 
 @Component({
   selector: 'app-delete-driver',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './delete-driver.component.html',
-  styleUrl: './delete-driver.component.css'
+  styleUrls: ['./delete-driver.component.css']
 })
 
+/**
+ * This component is responsible for removing a driver from the database.
+ * 
+ * This component provides a form for the user to select the driver to be deleted.
+ * Once the form is submitted, the information about the driver is used to identify the driver
+ * to be deleted. Upon successful deletion, the user is redirected to the "list-drivers" page.
+ * 
+ * @class DeleteDriverComponent
+ */
 export class DeleteDriverComponent {
 
+  /** Store the driver to be deleted */
   driver = { _id: '' };
-  drivers: any[] = [];
 
+  /** Store all drivers retrieved from the database */
+  drivers: Driver[] = [];
+
+  /**
+   * Constructor that injects the DriverService and Router for handling driver-related opeations 
+   * and to navigate between routes.
+   * 
+   * @param {DriverService} driversDB - Service used to perform database operations on driver.
+   * @param {Router} router - Router for navigating to other routes after successful operations.
+   */
   constructor(private driversDB: DriverService, private router: Router) { }
 
+  /**
+   * Called when the component is initialized. 
+   * Retrieves all drivers from the database and stores them in the drivers database.
+   * 
+   * @returns {void}
+   */
   ngOnInit(): void {
     this.driversDB.getDrivers().subscribe((data: any) => {
       for (let driver of data) {
@@ -25,8 +51,16 @@ export class DeleteDriverComponent {
       }
     })
   }
-
-  deleteDriver() {
+  
+  /**
+  * Deletes a driver from the database based on the driver mongooose ID.
+  * 
+  * When the driver is successfully deleted, it navigates the user to 'list-drivers' page.
+  * If an error occurs, it logs the error to the console and redirect user to invalid-data page.
+  * 
+  * @returns {void}
+  */
+  deleteDriver(): void {
     if (this.driver._id) {
       this.driversDB.removeDriver(this.driver._id).subscribe(
         (response: any) => {
