a1560dd6753c7b889fe9510397dba61435b069db
jtan0435 <jtan0435@student.monash.edu>
2024-10-03 18:32:33 +1000

    Added comments and reviewed the add driver component.

--- frontend/src/app/add-driver/add-driver.component.ts
+++ frontend/src/app/add-driver/add-driver.component.ts
@@ -12,18 +12,48 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./add-driver.component.css']
 })
 
+/**
+ * This component is responsible for adding a new driver to the database.
+ * 
+ * This component provides a form for the user to input details about the driver to be added. When the form
+ * is submitted, the details are sent to the server to be added to the database. Upon successful addition, 
+ * the user is navigated to the "list-drivers" page.
+ * 
+ * @class AddDriverComponent
+ */
 export class AddDriverComponent {
 
+  /**
+   * The Driver model is used to store details of the driver being added.
+   * This object will be bound to the form input fields in the template.
+   * 
+   * @type {Driver}
+   */
   driver: Driver = new Driver();
 
+  /**
+   * Constructor that injects the DriverService and Router for handling driver-related operations
+   * and to navigate between routes.
+   * 
+   * @param {DriverService} driversDB - Service used to perform database operations on drivers.
+   * @param {Router} router - Router for navigating to other routes after successful operations.
+   */
   constructor(private driversDB: DriverService, private router: Router) { }
 
-  addDriver() {
-    this.driversDB.addDriver(this.driver).subscribe(
-      response => {
+  /**
+   * Adds the current driver to the database by calling driverDB's addDriver method.
+   * 
+   * When the driver is successfully added, it navigates the user to 'list-drivers' page.
+   * If an error occurs, it navigates to the 'invalid-data' page and logs the error to the console.
+   * 
+   * @returns {void}
+   */
+  addDriver(): void {
+    this.driversDB.addDriver(this.driver).subscribe(() => {
         this.router.navigate(['list-drivers']);
       },
       error => {
+        this.router.navigate(['invalid-data']);
         console.error('Error adding driver:', error);
       }
     );
