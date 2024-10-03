4e16baf51c47f0c4ca222bc5fa50e1bce1e2fd53
jtan0435 <jtan0435@student.monash.edu>
2024-10-03 18:35:15 +1000

    Added comments and reviewed delete package component.

--- frontend/src/app/delete-package/delete-package.component.ts
+++ frontend/src/app/delete-package/delete-package.component.ts
@@ -11,20 +11,48 @@ import { Package } from '../models/package';
   templateUrl: './delete-package.component.html',
   styleUrl: './delete-package.component.css'
 })
+
+/**
+ * This component is responsible for deleting a package from the database.
+ * 
+ * This components provides a form for the user to choose the package to be deleted.
+ * After the form is submitted, the details are sent to the server to delete the package by
+ * its mongoose ID.
+ * 
+ * @class DeletePackageComponent
+ */
 export class DeletePackageComponent {
 
   package = { _id: '' };
   packages: Package[] = [];
 
+  /**
+   * Constructor that injects the PackageService and Router for handling package-related operations
+   * @param {PackageService} packagesDB 
+   * @param {Router} router - Router for navigating to other routes after successful operations.
+   */
   constructor(private packagesDB: PackageService, private router: Router) { }
 
+  /**
+   * Loads the packages from the database on initialization.
+   * 
+   * @returns {void}
+   */
   ngOnInit(): void {
     this.packagesDB.getPackages().subscribe((data: any) => {
       this.packages = data;
     })
   }
 
-  deletePackage() {
+  /**
+   * Deletes a package from the database based on the package mongoose ID.
+   * 
+   * When the package is successfully deleted, the user is redirected to the 'list-packages' page.
+   * If an error occurs, the error is logged to the console and the user is redirected to the 'invalid-data' page.
+   * 
+   * @returns {void}
+   */
+  deletePackage(): void {
     if (this.package._id) {
       this.packagesDB.removePackage(this.package._id).subscribe(
         (response: any) => {
