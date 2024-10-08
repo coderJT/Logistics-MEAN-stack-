fedafff72abf72e1ae35cfc7d014c354b2778711
jtan0435 <jtan0435@student.monash.edu>
2024-10-08 19:13:31 +1100

    Added comments to list drivers by department.

--- frontend/src/app/list-drivers-by-department/list-drivers-by-department.component.ts
+++ frontend/src/app/list-drivers-by-department/list-drivers-by-department.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DriverService } from '../driver.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Driver } from '../models/driver';
 
 @Component({
   selector: 'app-list-drivers-by-department',
@@ -11,17 +12,38 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './list-drivers-by-department.component.css'
 })
 
+/**
+ * This component is responsible for listing all drivers by their department.
+ * 
+ * This component uses the DriverService to retrieve drivers from the database and also
+ * provides functionality to delete drivers.
+ * 
+ * @class ListDriversByDepartmentComponent
+ */
 export class ListDriversByDepartmentComponent {
 
-  drivers: any[] = [];
+  /**
+   * The Driver array is used to store the drivers retrieved from the database.`
+   */
+  drivers: Driver[] = [];
   driver = { driver_department: "" }
 
+  /**
+   * Constructor that injects the DriverSevice and Router for handling driver-related operations 
+   * and to navigate between routes.
+   * @param driversDB 
+   * @param router 
+   */
   constructor(private driversDB: DriverService, private router: Router) { }
 
-  loadDrivers() {
-    this.drivers = [];
+  /**
+   * This method loads the drivers into the dirvers array from the database.
+   * 
+   * @returns {void}
+   */
+  loadDrivers(): void {
+    this.drivers = []; // reset the drivers array to prevent unnecessary duplication
     if (this.driver.driver_department) {
-      this.drivers = [];
       this.driversDB.getDriversByDepartment(this.driver.driver_department).subscribe((response: any) => {
         for (let driver of response) {
           this.drivers.push(driver);
@@ -30,7 +52,15 @@ export class ListDriversByDepartmentComponent {
     }
   }
 
-  onDelete(driverId: string) {
+/**
+ * This method deletes a driver from the database based on the driver mongoose ID by calling
+ * the removeDriver method of the DriverService.
+ * 
+ * @param driverId - ID of the driver to be deleted.
+ * 
+ * @returns {void}
+ */
+  onDelete(driverId: string): void {
     if (driverId) {
       this.driversDB.removeDriver(driverId).subscribe(
         (response: any) => {
@@ -41,6 +71,7 @@ export class ListDriversByDepartmentComponent {
           }
         },
         error => {
+          console.error('Error removing driver:', error);
           this.router.navigate(['invalid-data']);
         }
       );
