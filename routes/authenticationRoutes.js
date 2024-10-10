353966ee72a674ff66a1fb742142c5309aa06eb5
jtan0435 <jtan0435@student.monash.edu>
2024-10-10 14:29:12 +1100

    Added comments to authentication routes.

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -8,6 +8,7 @@
  * @requires express
  * @requires firebase-admin - Firebase admin SDK for Firestore database
  * @requires bcrypt - To encrypt user credentials.
+ * @requires jwt - Used to generate JWT tokens
  */
 
 // Import express library
@@ -25,94 +26,80 @@ const db = admin.firestore();
 // Import bcrypt library
 const bcrypt = require('bcrypt');
 
+// Import JWT library
+const jwt = require('jsonwebtoken');
+const JWT_KEY = "secret_key_in_env";
+
+const generateJWTToken = (username) => {
+    return jwt.sign({ username }, JWT_KEY, {expiresIn: '24h'});
+}
+
 /**
  * API Routes
  */
 
-/**
- * POST /api/v1/signup
- * 
- * @description Sign up a user through API.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.post('/api/v1/signup', async (req, res) => {
+ /**
+  * POST method to handle signup request.
+  * 
+  * @param {Object} req - Express request object.
+  * @param {Object} res - Express response object.
+  * 
+  * @returns {Promise<void>}
+  */
+ router.post('/api/v1/signup', async (req, res) => {
+     const { username, password, confirmPassword } = req.body;
 
-    const { username, password, confirmPassword } = req.body;
+     try {
+         const hashedPassword = await bcrypt.hash(password, 10);
+         await db.collection('users').doc(username).set({
+             username,
+             password: hashedPassword
+         });
 
-    if (password !== confirmPassword) {
-        return res.json({
-            error: "Password do not match."
-        })
-    }
+         res.json({ message: "User signed up successfully." });
+     } catch (error) {
+         res.status(500).json({ error: "Error during signup: " + error.message });
+     }
+ })
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+ /**
+  * POST method to handle login request.
+  * 
+  * @param {Object} req - Express request object.
+  * @param {Object} res - Express response object.
+  * 
+  * @returns {Promise<void>}
+  */
+ router.post('/api/v1/login', async (req, res) => {
+     const { username, password } = req.body;
 
-        await db.collection('users').doc(username).set({
-            username,
-            password: hashedPassword
-        });
-
-        res.json({
-            "status": "Signed up successfully."
-        })
-
-    } catch (error) {
-        res.json({
-            "status": error
-        })
-    }
-});
-
-/**
- * POST /api/v1/login
- * 
- * @description Log in a user through API.
- * 
- * @function
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.post('/api/v1/login', async (req, res) => {
-
-    const { username, password } = req.body;
-
-    try {
-        const userDoc = await db.collection('users').doc(username).get();
-
-        if (!userDoc.exists) {
-            return res.status(404).json({
-                "status": "User not found",
-            })
-        }
+     try {
+         const userDoc = await db.collection('users').doc(username).get();
+         if(!userDoc.exists){
+             return res.status(404).json({error: "User not found."});
+         }
 
         const userData = userDoc.data();
         const isMatch = await bcrypt.compare(password, userData.password);
 
-        if (!isMatch) {    
-            res.status(401).json({
-                "status": "Password incorrect.",
-            })
-        }
-        
-        req.session.user = { username }; 
+         if (!isMatch) {
+             return res.status(401).json({ error: "Incorrect password given. Please try again." });
+         }
 
-        res.status(200).json({
-            "status": "Logged in successfully."
-        })
+         const token = generateJWTToken(username);
 
-    } catch (error) {
-        res.json({
-            "status": error
-        })
-    }
-});
+         res.json({
+             message: "Logged in successfully.", token
+         })
+     } catch (error) {
+         res.status(500).json({
+             error: "Error during login:" + error.message
+         });
+     }
+ })
 
 /**
- * GET /api/v1/logout
+ * GET method to handle logout request.
  * 
  * @description Logs out current user through API. 
  * 
