feda8734cf17372b53a14c5f5d69a9c4f156268e
jtan0435 <jtan0435@student.monash.edu>
2024-10-10 14:27:41 +1100

    Added comments to both statistics and utils services.

--- frontend/src/app/statistics.service.ts
+++ frontend/src/app/statistics.service.ts
@@ -5,14 +5,29 @@ import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
-})
+})  
 
+/**
+ * Service that handles statistics (CRUD operation count).
+ * 
+ * @class StatisticsService
+ */
 export class StatisticsService {
 
   private apiUrl = 'http://localhost:8080/api/v1/statistics';
 
+  /**
+   * Constructor that injects the AuthenticationService for handling authentication-related operations,
+   * and also http client for making HTTP requests.
+   * @param {HttpClient} http - Http client
+   * @param {AuthenticationService} authService - Authentication Servicce
+   */
   constructor(private http: HttpClient, private authService: AuthenticationService) { }
 
+  /**
+   * Method that returns the driver and package count by calling 
+   * @returns {Observable<any>} - Driver and package count
+   */
   getStatistics(): Observable<any> {
     const headers = this.authService.setAuthHeaders(); 
     return this.http.get<any>(this.apiUrl, { headers });
--- frontend/src/app/utils.service.ts
+++ frontend/src/app/utils.service.ts
@@ -5,12 +5,28 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * Service for retrieving driver and package count.
+ * 
+ * @class UtilsService
+ */
 export class UtilsService {
 
   private apiUrl = "http://localhost:8080/api/v1/count";
   
+  /**
+   * Constructor that injects the HttpClient for making HTTP requests.
+   * 
+   * @param http - Http client
+   */
   constructor(private http: HttpClient) {}
 
+  /**
+   * Method that returns the driver and package count.
+   * 
+   * @returns {Observable<any>} - Driver and package count
+   */
   getCount(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
