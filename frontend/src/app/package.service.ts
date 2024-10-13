77ef49411d4c02e9a8125137fb3d993b4b28ab37
jtan0435 <jtan0435@student.monash.edu>
2024-10-13 16:14:01 +1100

    Updated to use localhost.

--- frontend/src/app/package.service.ts
+++ frontend/src/app/package.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class PackageService {
 
-  private apiUrl = 'http://10.148.0.2:8080/api/v1/packages'; 
+  private apiUrl = 'http://localhost:8080/api/v1/packages'; 
 
   constructor(private http: HttpClient, private authentication: AuthenticationService) { }
 
